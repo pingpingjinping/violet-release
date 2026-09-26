f188b4b5728798e0a953c4e739e08fb81249da8a
pingpingjinping <pingpingjinping@gmail.com>
2026-09-26 12:54:27 +0900

    Normalize spaces after emoji removal

--- violet/lib/services/download_archive.dart
+++ violet/lib/services/download_archive.dart
@@ -55,7 +55,7 @@ class DownloadArchive {
       }
     }
 
-    return buffer.toString().trim();
+    return buffer.toString().replaceAll(RegExp(r'\\s+'), ' ').trim();
   }
 
   static String _trimWindowsTrailingCharacters(String value) {
